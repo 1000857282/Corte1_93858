1a46f70561faab5d21c160a7598ec9da8453dca3
Nicol Castillo <ndcastillor@gmail.com>
2023-03-09 12:11:34 -0500

    Importar funciones a otro archivo

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+def fcn_factorial(x):
+    fact=1
+    #pass --> para que no aparezca error
+    for i in range(1,x+1):
+        fac*= i
+    return(fac)
+
+#Prueba uitaria : probar esta funcion , no se ejecuta este codigo en el otro archivo
+if __name__=="__main__":
+    fcn_factorial(7)
---
+++ P_factorial.py
@@ -0,0 +1,13 @@
+import Factorial as f
+#from Factorial impor fcn_factorial as f --> es otra forma de importar
+def main():
+    n = int(input("Ingrese el numero de grupos: "))
+    m = int(input("Ingrese el numero de muestras: "))
+    resultado = ((f(n))/(f(m)*f(n-m)))
+    print(f"El numero de combinaciones posibles es: {resultado}")
+
+    result =(f.fcn_factorial)
+
+
+if __name__=="__main__":
+    main()
